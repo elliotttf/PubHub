f5ba4d1f5cfd47c576d5cc56fa51a5878e825c0e
Elliott Foster <elliott@fourkitchens.com>
2012-01-11 03:46:00 +0000

    Making PubHub work with builtin cluster.

--- app.js
+++ app.js
@@ -1,58 +1,90 @@
+/**
+ * @fileoverview Base for PubHub.
+ */
 
 /**
  * Module dependencies.
  */
 
+var cluster = require('cluster');
 var cp = require('child_process');
-var form = require('connect-form');
 var events = require('events');
 var express = require('express');
+var Factory = require('./factory.js').Factory;
+var form = require('connect-form');
+var mongoose = require('mongoose');
 var routes = require('./routes');
 
-var app = module.exports = express.createServer(
-  form({ keepExtensions: true })
-);
+if (cluster.isMaster) {
+  // Start the factory!
+  mongoose.connect('mongodb://localhost/pubhub');
+  mongoose.connection.on('error', function(err) {
+      console.error(err);
+  });
+  var factory = new Factory();
 
-// Configuration
+  // TODO - make this variable.
+  for (var i = 0; i < 1; i++) {
+    var worker = cluster.fork();
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+    worker.on('message', function onMessage(msg) {
+      if (msg.query) {
+        factory.subscribe(msg.query);
+      }
+      else if (msg.feed) {
+        factory.publish(msg.feed);
+      }
+    });
+  }
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+  cluster.on('death', function onDeath(worker) {
+    console.log('Worker ' + worker.pid + ' died. Restarting.');
+    cluster.fork();
+  });
+}
+else  {
+  var app = module.exports = express.createServer(
+    form({ keepExtensions: true })
+  );
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+  // Configuration
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
 
-// Start the hub factory.
-var factory = cp.fork('./factory.js');
-var hubEvents = new events.EventEmitter();
-hubEvents.on('subscribed', function onSubscribed(query) {
-  factory.send({ 'subscribed': query });
-});
-hubEvents.on('published', function onPublished(feed) {
-  factory.send({ 'published': feed });
-});
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
 
-// Routes
-app.get('/', routes.index);
-app.get('/subscribe', function onGet(req, res) {
-  res.send('Only POST subscriptions are supported.');
-});
-app.post('/subscribe', function onSubscribe(req, res) {
-  routes.subscribe(req, res, hubEvents);
-});
-app.post('/publish', function onPublish(req, res) {
-  routes.publish(req, res, hubEvents);
-});
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+  });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  var hubEvents = new events.EventEmitter();
+  hubEvents.on('subscribed', function onSubscribed(query) {
+    process.send({ 'query': query });
+  });
+  hubEvents.on('published', function onPublished(feed) {
+    process.send({ 'feed': feed });
+  });
+
+  // Routes
+  app.get('/', routes.index);
+  app.get('/subscribe', function onGet(req, res) {
+    res.send('Only POST subscriptions are supported.');
+  });
+  app.post('/subscribe', function onSubscribe(req, res) {
+    routes.subscribe(req, res, hubEvents);
+  });
+  app.post('/publish', function onPublish(req, res) {
+    routes.publish(req, res, hubEvents);
+  });
+
+  app.listen(3000);
+}
+
